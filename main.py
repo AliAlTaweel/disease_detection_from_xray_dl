5d1b61f795a747f73900bbcfff03c466d76792bc
alihaltaweel <alihaltaweel89@gmail.com>
2026-01-20 15:50:57 +0200

    update main

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+if __name__ == "__main__":
+    print("🚀 Starting Pneumonia Detection System...")
+    # This command tells your computer to run the Streamlit script inside src/
+    os.system("streamlit run src/main.py")
